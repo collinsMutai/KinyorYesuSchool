eddd9c8d4ae2ffc1e423e8b4c775bcc79e630729
Collins Mutai <collinsfrontend@gmail.com>
2024-12-03 00:00:41 +0300

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
 import { RouterModule } from '@angular/router';
+import { WhychooseusComponent } from '../whychooseus/whychooseus.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SliderComponent, RouterModule],
+  imports: [CommonModule, SliderComponent, RouterModule, WhychooseusComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
