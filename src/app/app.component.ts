f75b6de05415ed5b43286ea08f7401bab870c06e
Collins Mutai <collinsfrontend@gmail.com>
2024-12-02 16:54:13 +0300

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, TopnavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, ActivatedRoute, RouterModule } from '@angular/router';
 import { TopnavComponent } from '../topnav/topnav.component';
 
@@ -11,6 +11,7 @@ import { TopnavComponent } from '../topnav/topnav.component';
 })
 export class NavbarComponent {
   activeLink: string = '';
+  lastScrollTop = 0; // To track scroll direction
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.router.events.subscribe(() => {
@@ -18,15 +19,37 @@ export class NavbarComponent {
     });
   }
 
+  // Scroll event listener
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScroll =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    // If scrolling down, reduce logo size
+    if (currentScroll > this.lastScrollTop) {
+      // Scroll down, shrink the logo
+      document.querySelector('.navbar-brand')?.classList.add('shrink');
+    } else {
+      // Scroll up, return to default size
+      document.querySelector('.navbar-brand')?.classList.remove('shrink');
+    }
+
+    // Update the last scroll position
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  }
+
+  // Update active link based on the current route
   setActiveLink() {
     const currentRoute = this.router.url.split('/')[1];
     this.activeLink = currentRoute || 'home';
   }
 
+  // Check if the route is active
   isActive(route: string): boolean {
     return this.activeLink === route;
   }
 
+  // Close the menu on link click
   closeMenu() {
     const navbarToggler = document.querySelector(
       '.navbar-toggler'
