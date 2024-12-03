f7a07c342952816a30e3f7221d7edd40470aeaac
Collins Mutai <collinsfrontend@gmail.com>
2024-12-03 22:44:10 +0300

    ui

--- dist/kinyoryesuschool/server/main.server.mjs
+++ dist/kinyoryesuschool/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OBVL4AK6.mjs";import"./chunk-DHJ3N4VP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TU72S2VC.mjs";import"./chunk-DHJ3N4VP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
