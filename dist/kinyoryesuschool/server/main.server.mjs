bb66554d774fd34a8fb2dc2dbab49a8e87467e4c
Collins Mutai <collinsfrontend@gmail.com>
2024-12-05 14:25:55 +0300

    ui

--- dist/kinyoryesuschool/server/main.server.mjs
+++ dist/kinyoryesuschool/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MAI7NMZW.mjs";import"./chunk-DHJ3N4VP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-37DAB6HH.mjs";import"./chunk-BDWA6LJN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { TopnavComponent } from './components/topnav/topnav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, TopnavComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, TopnavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
