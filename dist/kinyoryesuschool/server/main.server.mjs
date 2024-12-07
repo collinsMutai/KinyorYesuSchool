ce57958a42bf77c76303055833c3d4581d823898
Collins Mutai <collinsfrontend@gmail.com>
2024-12-07 17:39:46 +0300

    ui

--- dist/kinyoryesuschool/server/main.server.mjs
+++ dist/kinyoryesuschool/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XRLBCNJA.mjs";import"./chunk-4AYAT7DU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ACINUBBH.mjs";import"./chunk-ZSYWOK7M.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.css'
+  styleUrls: ['./gallery.component.css'],
+  imports: [CommonModule],
 })
 export class GalleryComponent {
+  // Array of image objects with source and alt text
+  images = [
+    { src: 'assets/kinyoryesu2.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu3.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu4.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu5.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu6.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu8.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu9.jpeg', alt: 'Image 1' },
+    { src: 'assets/kinyoryesu10.jpeg', alt: 'Image 1' },
+  ];
 
+  
 }
